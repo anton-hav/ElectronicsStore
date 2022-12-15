2b7416a2d735a98b8d971550ea4f47a1bae31a3b
anton-hav <anton.havrilenia@hotmail.com>
2022-12-15 15:00:36 +0100

    adds new migration that add refresh token

--- WebAPI/ElectronicsStore.DataBase/ElectronicsStoreDbContext.cs
+++ WebAPI/ElectronicsStore.DataBase/ElectronicsStoreDbContext.cs
@@ -7,6 +7,7 @@ public class ElectronicsStoreDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Role> Roles { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
