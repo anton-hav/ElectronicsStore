7adf55892e6ceaa790b6c9ec88425508e34ec49e
anton-hav <anton.havrilenia@hotmail.com>
2022-12-26 22:54:03 +0100

    adds new migration that add Category to the database

--- WebAPI/ElectronicsStore.DataBase/Migrations/20221226201345_AddCategoryToDatabase.Designer.cs
+++ WebAPI/ElectronicsStore.DataBase/Migrations/20221226201345_AddCategoryToDatabase.Designer.cs
@@ -4,6 +4,7 @@ using ElectronicsStore.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ElectronicsStore.DataBase.Migrations
 {
     [DbContext(typeof(ElectronicsStoreDbContext))]
-    partial class ElectronicsStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221226201345_AddCategoryToDatabase")]
+    partial class AddCategoryToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
